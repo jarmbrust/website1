6bc27f6a92c5429a3300fc80d7f49a45292739c8
James Armbrust <james.armbrust@gmail.com>
2024-11-19 11:09:26 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import NavBar from '@/components/NavBar.vue';
+</script>
+
+<template>
+  <header>
+    <NavBar />
+  </header>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+#app {
+  header {
+    line-height: 1.5;
+    border-bottom:rgb(195, 201, 220) 1px solid;
+  }
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <nav>
+    <div class="myname">James Armbrust</div>
+    <div class="routes">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/blog">Blog</RouterLink>
+      <RouterLink to="/resume">Resume</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  font-size: 22px;
+  margin: 10px 0;
+  height: 45px;
+  display: flex;
+  justify-content: space-between;
+  .myname {
+    font-weight: 700;
+    font-size: 30px;
+  }
+  .routes {
+    a.router-link-exact-active {
+      color: #9cb6c9;
+    }
+    a:hover {
+      color: #849faf;
+    }
+    a:last-of-type {
+      padding: 0 0 0 18px;
+    }
+    a {
+      padding: 0 18px;
+      text-decoration: none;
+      color: #f8f8f8;
+    }
+  }
+}
+</style>
