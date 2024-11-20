86fc3bf9707674ca6231eb8201f0aca1690488c8
James Armbrust <james.armbrust@gmail.com>
2024-11-20 10:26:24 -0700

    routiing refresh

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,31 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/blog', component: () => import('@/views/BlogView.vue'), },
-  { path: '/resume', component: () => import('@/views/ResumeView.vue'), },
-  { path: '/about', component: () => import('@/views/AboutView.vue'), },
+  {
+    path: '/',
+    component: HomeView
+  },
+  {
+    path: '/blog',
+    component: () => import('@/views/BlogView.vue'),
+    meta: {
+      redirectOnRefresh: '/blog'
+    }
+  },
+  {
+    path: '/resume',
+    component: () => import('@/views/ResumeView.vue'),
+    meta: {
+      redirectOnRefresh: '/resume'
+    }
+  },
+  {
+    path: '/about',
+    component: () => import('@/views/AboutView.vue'),
+    meta: {
+      redirectOnRefresh: '/about'
+    }
+  },
 ];
 
 const router = createRouter({
