36fd97a5cf24618a3f21daadaa616fa8ed762059
James Armbrust <james.armbrust@gmail.com>
2024-12-16 04:43:15 -0700

    fixed footer

--- src/App.vue
+++ src/App.vue
@@ -20,23 +20,16 @@ import SiteFooter from './components/SiteFooter.vue';
 @use '@/assets/_variables.scss';
 
 #app {
-  height: 100vh;
-  background-color: variables.$background-color;
-  z-index: 98;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  margin: 0;
   header {
     line-height: 1.5;
     border-bottom:rgb(195, 201, 220) 1px solid;
-    // position: sticky;
-    top: 0;
-    z-index: 100;
   }
   footer {
-    display: flex;
-    flex-direction: column;
-    // position: sticky;
-    bottom: 0;
-    height: 100vh;
-    z-index: 99;
+    margin-top: auto;
   }
 }
 </style>
--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,18 +1,5 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div class="copyright">
     <p>James Armbrust © 2024</p>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.copyright {
-  display: flex;
-  width: 100%;
-  margin-top: auto;
-  border-top: rgb(195, 201, 220) 1px solid;
-}
-</style>
